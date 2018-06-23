3ccc4395b3c173873ed588636c2339d7c95e9a88
Maria Jose Ortuzar <re4ortuzar@hotmail.com>
2018-06-22 20:40:17 -0400

    actualizacion responsive mobile

---
+++ js/index.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+    var domAttributesJavascript = anime({
+        targets: '#domAttributesJavascript input',
+        value: 80+"%",
+        round: 1,
+        easing: 'easeInOutExpo'
+    });
+    var domAttributesAngular = anime({
+        targets: '#domAttributesAngular input',
+        value: 40+"%",
+        round: 1,
+        easing: 'easeInOutExpo'
+    });
+    var domAttributesjQuery = anime({
+        targets: '#domAttributesjQuery input',
+        value: 80+"%",
+        round: 1,
+        easing: 'easeInOutExpo'
+    });
+    var domAttributesGit = anime({
+        targets: '#domAttributesGit input',
+        value: 65+"%",
+        round: 1,
+        easing: 'easeInOutExpo'
+    });
+    var domAttributesMaterialize = anime({
+        targets: '#domAttributesMaterialize input',
+        value: 60+"%",
+        round: 1,
+        easing: 'easeInOutExpo'
+    });
+    var domAttributesBootstrap = anime({
+        targets: '#domAttributesBootstrap input',
+        value: 68+"%",
+        round: 1,
+        easing: 'easeInOutExpo'
+    });
+});
